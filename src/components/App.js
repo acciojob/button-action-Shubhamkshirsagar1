2a60d43bc4d9cc14a13ee5a16ecbe9a4edb3e8a9
shubham kshirsagar <117492782+Shubhamkshirsagar1@users.noreply.github.com>
2023-02-16 08:34:57 +0530

    DoneAgain

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,20 @@ import React, { useState } from "react";
 import "./../styles/App.css";
 
 const App = (props) => {
-  const clickHandler = () => {
-    document.getElementById("para").style.display = "block";
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleOnClick = () => {
+    setIsClicked(true)
   };
 
   return (
-    <div className="App" id="main">
+    <div id="main">
       <p id="para">
-        Hello, I've learnt to use the full-stack evaluation tool. This makes me
-        so happy
+        {isClicked
+          ? "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+          : ""}
       </p>
-      <button id="click"></button>
-      <button id="click" onClick={clickHandler}>
-        Click me
-      </button>
+      <button id="click" onClick={handleOnClick}>Click Me</button>
     </div>
   );
 };
